7f925ca850c649f1b4fe9cad713e587d2d40fbdc
Samuel Lambert <samuel.lambert@hotmail.com>
2017-12-26 09:41:45 -0500

    Added the state or province field in the JSON data

--- data/jsonify_tsv_data.py
+++ data/jsonify_tsv_data.py
@@ -1,22 +1,64 @@
 #!/usr/bin/env python3
 
+'''
+Parses the TSV data then generates a JSON file only
+containing the data needed for auto-completion.
+
+This relevant data is:
+- City name
+- Country
+- Latitude
+- Longitude
+
+The format on the TSV file is the following:
+id name ascii alt_name lat long feat_class feat_code country ...
+'''
+
 import json
 
 INPUT_FILE = 'cities_canada-usa.tsv'
 OUTPUT_FILE = 'cities_canada-usa.json'
 
+def resolve_state(token, country):
+    '''
+    Retrieves the two-letter state/province code, considering
+    that provinces are numerically coded.
+
+    For some reason, code 06 is not being used.
+    '''
+    if country == 'us':
+        return token.lower()
+
+    if token == '01':
+        return 'ab'
+    elif token == '02':
+        return 'bc'
+    elif token == '03':
+        return 'mb'
+    elif token == '04':
+        return 'nb'
+    elif token == '05':
+        return 'nl'
+    elif token == '07':
+        return 'ns'
+    elif token == '08':
+        return 'on'
+    elif token == '09':
+        return 'pe'
+    elif token == '10':
+        return 'qc'
+    elif token == '11':
+        return 'sk'
+    elif token == '12':
+        return 'yt'
+    elif token == '13':
+        return 'nt'
+    elif token == '14':
+        return 'nu'
+    else:
+        raise ValueError('Unknown state/province value')
+
 def main():
-    '''
-    This parses the TSV data then generates a JSON file
-    only containing the data needed for auto-completion.
-
-    This relevant data is: 
-    - City name
-    - Country
-    - Latitude
-    - Longitude
-    '''
-
     cities_list = []
 
     with open(INPUT_FILE) as input_file:
@@ -24,11 +66,19 @@ def main():
 
         for line in input_file:
             tokens = line.split('\t')
+
+            name = tokens[1].lower()
+            country = tokens[8].lower()
+            state = resolve_state(tokens[10], country)
+            latitude = float(tokens[4])
+            longitude = float(tokens[5])
+
             city = {
-                'name': tokens[1].lower(),
-                'country': tokens[8].lower(),
-                'lat': float(tokens[4]),
-                'long': float(tokens[5])
+                'name': name,
+                'state': state,
+                'country': country,
+                'latitude': latitude,
+                'longitude': longitude
             }
 
             cities_list.append(city)
