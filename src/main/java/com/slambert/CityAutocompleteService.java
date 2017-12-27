77960cc06ad4b75a9c9f9c167a7d9dcf89e3efea
Samuel Lambert <samuel.lambert@hotmail.com>
2017-12-27 14:53:40 -0500

    Added maven project skeleton

---
+++ src/main/java/com/slambert/CityAutocompleteService.java
@@ -0,0 +1,7 @@
+package com.slambert;
+
+public class CityAutocompleteService {
+    public static void main(final String[] args) {
+        // TODO
+    }
+}
---
+++ src/test/java/com/slambert/CityAutocompleteServiceTest.java
@@ -0,0 +1,7 @@
+package com.slambert;
+
+import junit.framework.TestCase;
+
+public class CityAutocompleteServiceTest extends TestCase {
+    // TODO
+}
