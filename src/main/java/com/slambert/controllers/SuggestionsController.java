a9a7bb8ff0d27187910eb3d50eb0172de96af627
Samuel Lambert <samuel.lambert@hotmail.com>
2017-12-28 15:29:31 -0500

    Added a configuration mechanism
    Added class ConfigurationManager that deals with service configuration.
    Values of options can ben changed in the application.properties file in
    the root folder.

--- src/main/java/com/slambert/controllers/SuggestionsController.java
+++ src/main/java/com/slambert/controllers/SuggestionsController.java
@@ -8,7 +8,9 @@
 
 package com.slambert.controllers;
 
+import com.slambert.model.ConfigurationManager;
 import com.slambert.model.SuggestionsQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,15 +23,20 @@ public class SuggestionsController {
 
     private static final String SUGGESTIONS_PATH = "/suggestions";
 
+    @Autowired
+    private ConfigurationManager configurationManager;
+
     @RequestMapping(value = SUGGESTIONS_PATH, method = RequestMethod.GET)
     public SuggestionsQuery getSuggestions(@RequestParam final String q,
                                            @RequestParam final Optional<Double> latitude,
                                            @RequestParam final Optional<Double> longitude) {
         // Example query: GET /suggestions?q=Londo&latitude=43.70011&longitude=-79.4163
 
-        // TODO: retrieve default latitude/longitude from configuration file.
-        final Double sanitizedLatitude = latitude.isPresent() ? latitude.get() : 0.0;
-        final Double sanitizedLongitude = longitude.isPresent() ? longitude.get() : 0.0;
+        final Double sanitizedLatitude =
+                latitude.isPresent() ? latitude.get() : configurationManager.getFallbackLatitude();
+        final Double sanitizedLongitude =
+                longitude.isPresent() ? longitude.get() : configurationManager.getFallbackLongitude();
+
         return new SuggestionsQuery(q, sanitizedLatitude, sanitizedLongitude);
     }
 
---
+++ src/main/java/com/slambert/model/ConfigurationManager.java
@@ -0,0 +1,48 @@
+// Coveo Backend Coding Challenge
+// Design an API endpoint that provides auto-complete suggestions for large cities.
+//
+// Copyright (c) 2017 Samuel Lambert
+//
+// This software may be modified and distributed under the terms
+// of the MIT license. See the LICENSE file for details.
+
+package com.slambert.model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties
+@PropertySource("file:application.properties")
+public class ConfigurationManager {
+
+    private Boolean retrieveUserLocation;
+    private Double fallbackLatitude;
+    private Double fallbackLongitude;
+
+    public Boolean isRetrievingUserLocation() {
+        return retrieveUserLocation;
+    }
+
+    public void setRetrieveUserLocation(final Boolean retrieveUserLocation) {
+        this.retrieveUserLocation = retrieveUserLocation;
+    }
+
+    public Double getFallbackLatitude() {
+        return fallbackLatitude;
+    }
+
+    public void setFallbackLatitude(final Double fallbackLatitude) {
+        this.fallbackLatitude = fallbackLatitude;
+    }
+
+    public Double getFallbackLongitude() {
+        return fallbackLongitude;
+    }
+
+    public void setFallbackLongitude(final Double fallbackLongitude) {
+        this.fallbackLongitude = fallbackLongitude;
+    }
+
+}
