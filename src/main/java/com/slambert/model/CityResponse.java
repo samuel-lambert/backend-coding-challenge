3ecf366426be47d6a4bb8121215298942ff55fb2
Samuel Lambert <samuel.lambert@hotmail.com>
2017-12-29 15:51:57 -0500

    Added the CityResponse class
    This class will act as a mapper to a city suggestion and its JSON
    counterpart. It inherits Comparable so we can sort results in sorted
    sets according to confidence scores.

---
+++ src/main/java/com/slambert/model/CityResponse.java
@@ -0,0 +1,56 @@
+// Coveo Backend Coding Challenge
+// Design an API endpoint that provides auto-complete suggestions for large cities.
+//
+// Copyright (c) 2017 Samuel Lambert
+//
+// This software may be modified and distributed under the terms
+// of the MIT license. See the LICENSE file for details.
+
+package com.slambert.model;
+
+public class CityResponse implements Comparable<CityResponse> {
+    private final String name;
+    private final Double latitude;
+    private final Double longitude;
+    private final Double score;
+
+    public CityResponse(final City city, final Double score) {
+        // Formatting of the city name as follow: "Montréal, QC, CA"
+        this.name = city.getName().substring(0, 1).toUpperCase() +
+                city.getName().substring(1) +
+                ", " +
+                city.getState().toUpperCase() +
+                ", " +
+                city.getCountry().toUpperCase();
+
+        this.latitude = city.getLatitude();
+        this.longitude = city.getLongitude();
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    @Override
+    public int compareTo(final CityResponse rhs) {
+        if (score == rhs.score) {
+            return name.compareTo(rhs.name);
+        }
+
+        return score.compareTo(rhs.score);
+    }
+
+}
